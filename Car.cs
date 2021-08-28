dc634052732e4bb9a83abc06d11fdd042d2840a4
Daniel Aguirre <uhyeay24@gmail.com>
2021-08-28 08:03:55 -0500

    initial commit

---
+++ Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExercise
+{
+    public class Car
+    {
+        public string Make;
+        public string Model;
+        public string Year;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var carOne = new Car();
+            carOne.Make = "I don't know cars like that";
+            carOne.Model = "PT Cruiser";
+            carOne.Year = "1994";
+
+        }
+    }
+}
